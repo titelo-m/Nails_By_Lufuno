b7180b65f8285bbc5de8d5a7342cc827e6b73b6e
Titelo Maleka <titelo_maleka@icloud.com>
2026-01-17 01:33:23 +0200

    Fix GA tracking using react-gtag for SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,29 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
-declare global {
-  interface Window {
-    dataLayer: any[];
-  }
-}
+import ReactGA from "react-gtag";
 
 const queryClient = new QueryClient();
 
-const App = () => {
+const GA_MEASUREMENT_ID = "G-CWEG31JSDT";
+
+// Track page views on route change
+const GAListener = () => {
+  const location = useLocation();
 
   useEffect(() => {
-    // 1. Add GA script to head
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-CWEG31JSDT";
-    script.async = true;
-    document.head.appendChild(script);
+    ReactGA.pageview(location.pathname);
+  }, [location]);
 
-    // 2. Initialize dataLayer and gtag function
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]) {
-      window.dataLayer.push(args);
-    }
+  return null;
+};
 
-    // 3. Configure GA with your Measurement ID
-    gtag("js", new Date());
-    gtag("config", "G-CWEG31JSDT", {
-      page_path: window.location.pathname,
-    });
+const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(GA_MEASUREMENT_ID);
   }, []);
 
   return (
@@ -43,9 +34,9 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <GAListener />
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
